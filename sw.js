74276361a5a126fdd614be08915bf97439c333b9
Saitama21 <62865879+Saitama21@users.noreply.github.com>
2026-09-16 01:24:02 +0300

    chore: refresh offline cache for v0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='cnc-828-v1';
-const FILES=['./','./index.html','./src/styles.css','./src/app.js','./manifest.webmanifest','./icons/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='cnc-828-v2';
+const FILES=['./','./index.html','./src/styles.css','./src/v02.css','./src/app.js','./manifest.webmanifest','./icons/icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
